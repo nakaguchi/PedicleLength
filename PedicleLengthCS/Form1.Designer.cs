baaca159f36c1e591e23a35d2919ac5fcbe287c6
nakaguchi <nakaguchi@faculty.chiba-u.jp>
2020-09-11 09:45:20 +0900

    クリック点の距離算出まで実装

--- PedicleLengthCS/Form1.Designer.cs
+++ PedicleLengthCS/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace PedicleLengthCS {
-    partial class Form1 {
+    partial class MainForm {
         /// <summary>
         /// 必要なデザイナー変数です。
         /// </summary>
@@ -23,47 +23,235 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnOpenDicom = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TbrSliceIdx = new System.Windows.Forms.TrackBar();
+            this.LblSliceIdx = new System.Windows.Forms.Label();
+            this.LblWindowLevel = new System.Windows.Forms.Label();
+            this.TbrWindowLevel = new System.Windows.Forms.TrackBar();
+            this.LblWindowWidth = new System.Windows.Forms.Label();
+            this.TbrWindowWidth = new System.Windows.Forms.TrackBar();
+            this.LbxPoints = new System.Windows.Forms.ListBox();
+            this.LblVoxel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblLength = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblCursor = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbrSliceIdx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbrWindowLevel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbrWindowWidth)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // BtnOpenDicom
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BtnOpenDicom.Location = new System.Drawing.Point(12, 12);
+            this.BtnOpenDicom.Name = "BtnOpenDicom";
+            this.BtnOpenDicom.Size = new System.Drawing.Size(161, 31);
+            this.BtnOpenDicom.TabIndex = 0;
+            this.BtnOpenDicom.Text = "Open Dicom Folder";
+            this.BtnOpenDicom.UseVisualStyleBackColor = true;
+            this.BtnOpenDicom.Click += new System.EventHandler(this.BtnOpenDicom_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(101, 12);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(179, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.Size = new System.Drawing.Size(512, 510);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
-            // Form1
+            // TbrSliceIdx
+            // 
+            this.TbrSliceIdx.Location = new System.Drawing.Point(12, 106);
+            this.TbrSliceIdx.Name = "TbrSliceIdx";
+            this.TbrSliceIdx.Size = new System.Drawing.Size(161, 45);
+            this.TbrSliceIdx.TabIndex = 2;
+            this.TbrSliceIdx.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.TbrSliceIdx.ValueChanged += new System.EventHandler(this.SliderChanged);
+            // 
+            // LblSliceIdx
+            // 
+            this.LblSliceIdx.AutoSize = true;
+            this.LblSliceIdx.Location = new System.Drawing.Point(13, 94);
+            this.LblSliceIdx.Name = "LblSliceIdx";
+            this.LblSliceIdx.Size = new System.Drawing.Size(61, 12);
+            this.LblSliceIdx.TabIndex = 3;
+            this.LblSliceIdx.Text = "Slice index";
+            // 
+            // LblWindowLevel
+            // 
+            this.LblWindowLevel.AutoSize = true;
+            this.LblWindowLevel.Location = new System.Drawing.Point(13, 140);
+            this.LblWindowLevel.Name = "LblWindowLevel";
+            this.LblWindowLevel.Size = new System.Drawing.Size(74, 12);
+            this.LblWindowLevel.TabIndex = 5;
+            this.LblWindowLevel.Text = "Window Level";
+            // 
+            // TbrWindowLevel
+            // 
+            this.TbrWindowLevel.LargeChange = 10;
+            this.TbrWindowLevel.Location = new System.Drawing.Point(12, 152);
+            this.TbrWindowLevel.Maximum = 2000;
+            this.TbrWindowLevel.Minimum = -2000;
+            this.TbrWindowLevel.Name = "TbrWindowLevel";
+            this.TbrWindowLevel.Size = new System.Drawing.Size(161, 45);
+            this.TbrWindowLevel.TabIndex = 4;
+            this.TbrWindowLevel.TickFrequency = 100;
+            this.TbrWindowLevel.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.TbrWindowLevel.ValueChanged += new System.EventHandler(this.SliderChanged);
+            // 
+            // LblWindowWidth
+            // 
+            this.LblWindowWidth.AutoSize = true;
+            this.LblWindowWidth.Location = new System.Drawing.Point(13, 185);
+            this.LblWindowWidth.Name = "LblWindowWidth";
+            this.LblWindowWidth.Size = new System.Drawing.Size(75, 12);
+            this.LblWindowWidth.TabIndex = 7;
+            this.LblWindowWidth.Text = "Window Width";
+            // 
+            // TbrWindowWidth
+            // 
+            this.TbrWindowWidth.LargeChange = 10;
+            this.TbrWindowWidth.Location = new System.Drawing.Point(12, 198);
+            this.TbrWindowWidth.Maximum = 3000;
+            this.TbrWindowWidth.Minimum = 1;
+            this.TbrWindowWidth.Name = "TbrWindowWidth";
+            this.TbrWindowWidth.Size = new System.Drawing.Size(161, 45);
+            this.TbrWindowWidth.TabIndex = 6;
+            this.TbrWindowWidth.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.TbrWindowWidth.Value = 300;
+            this.TbrWindowWidth.ValueChanged += new System.EventHandler(this.SliderChanged);
+            // 
+            // LbxPoints
+            // 
+            this.LbxPoints.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LbxPoints.Font = new System.Drawing.Font("ＭＳ ゴシック", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LbxPoints.FormattingEnabled = true;
+            this.LbxPoints.Location = new System.Drawing.Point(15, 268);
+            this.LbxPoints.Name = "LbxPoints";
+            this.LbxPoints.Size = new System.Drawing.Size(158, 199);
+            this.LbxPoints.TabIndex = 10;
+            // 
+            // LblVoxel
+            // 
+            this.LblVoxel.AutoSize = true;
+            this.LblVoxel.Location = new System.Drawing.Point(12, 50);
+            this.LblVoxel.Name = "LblVoxel";
+            this.LblVoxel.Size = new System.Drawing.Size(34, 12);
+            this.LblVoxel.TabIndex = 11;
+            this.LblVoxel.Text = "Voxel";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Length";
+            // 
+            // LblLength
+            // 
+            this.LblLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LblLength.AutoSize = true;
+            this.LblLength.Font = new System.Drawing.Font("ＭＳ ゴシック", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LblLength.Location = new System.Drawing.Point(55, 497);
+            this.LblLength.Name = "LblLength";
+            this.LblLength.Size = new System.Drawing.Size(102, 27);
+            this.LblLength.TabIndex = 13;
+            this.LblLength.Text = "   0.0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(150, 509);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "mm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Point list";
+            // 
+            // LblCursor
+            // 
+            this.LblCursor.AutoSize = true;
+            this.LblCursor.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LblCursor.Location = new System.Drawing.Point(12, 66);
+            this.LblCursor.Name = "LblCursor";
+            this.LblCursor.Size = new System.Drawing.Size(39, 12);
+            this.LblCursor.TabIndex = 16;
+            this.LblCursor.Text = "Cursor";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 540);
+            this.ClientSize = new System.Drawing.Size(703, 534);
+            this.Controls.Add(this.LblCursor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LblLength);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblVoxel);
+            this.Controls.Add(this.LbxPoints);
+            this.Controls.Add(this.LblWindowWidth);
+            this.Controls.Add(this.TbrWindowWidth);
+            this.Controls.Add(this.LblWindowLevel);
+            this.Controls.Add(this.TbrWindowLevel);
+            this.Controls.Add(this.LblSliceIdx);
+            this.Controls.Add(this.TbrSliceIdx);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.BtnOpenDicom);
+            this.Name = "MainForm";
+            this.Text = "Pedicle Length";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbrSliceIdx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbrWindowLevel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbrWindowWidth)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnOpenDicom;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar TbrSliceIdx;
+        private System.Windows.Forms.Label LblSliceIdx;
+        private System.Windows.Forms.Label LblWindowLevel;
+        private System.Windows.Forms.TrackBar TbrWindowLevel;
+        private System.Windows.Forms.Label LblWindowWidth;
+        private System.Windows.Forms.TrackBar TbrWindowWidth;
+        private System.Windows.Forms.ListBox LbxPoints;
+        private System.Windows.Forms.Label LblVoxel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblLength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblCursor;
     }
 }
 
--- PedicleLengthCS/Form1.cs
+++ PedicleLengthCS/Form1.cs
@@ -2,43 +2,218 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
 using Dicom;
 using Dicom.Imaging;
-using System.Diagnostics;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
 
 namespace PedicleLengthCS {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class MainForm : Form {
+
+        public const int ImgW = 512;
+        public const int ImgH = 512;
+        public const int PointSize = 4;
+        private List<Mat> _Slices = new List<Mat>();
+        private List<Point3i> _Points = new List<Point3i>();
+        int _idx;
+        int _wl;
+        int _ww;
+        double _SliceThickness = -1.0;
+        double _PixelSpacingX = -1.0;
+        double _PixelSpacingY = -1.0;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainForm() {
             InitializeComponent();
+            pictureBox1.MouseWheel += new MouseEventHandler(pictureBox1_MouseWheel);
+            _idx = TbrSliceIdx.Value;
+            _wl = TbrWindowLevel.Value;
+            _ww = TbrWindowWidth.Value;
         }
 
-        private void button1_Click(object sender, EventArgs e) {
+        /// <summary>
+        /// Dicomファイルの読み込み
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        private bool ReadDicom(string dir) {
+            var dicomFiles = Directory.GetFiles(dir, @"*", SearchOption.TopDirectoryOnly);
 
-            //var file = DicomFile.Open(@"D:\usr\prog\PedicleLength\SampleData\5mm early phase\pedicle length 5mm early001");
-            //var img = new DicomImage(file.Dataset);
-            //var imgd = img.RenderImage(0).Pixels;
+            _Slices.Clear();
+            foreach (var f in dicomFiles) {
+                var dicomImg = new DicomImage(f);
+                if (dicomImg == null) continue;
+                if (_Slices.Count < 1) {
+                    _SliceThickness = dicomImg.Dataset.GetValue<double>(new DicomTag(0x18, 0x50), 0);
+                    _PixelSpacingX = dicomImg.Dataset.GetValue<double>(new DicomTag(0x28, 0x30), 0);
+                    _PixelSpacingY = dicomImg.Dataset.GetValue<double>(new DicomTag(0x28, 0x30), 1);
+                }
+                _Slices.Add(new Mat(ImgH, ImgW, MatType.CV_16SC1, dicomImg.PixelData.GetFrame(0).Data));
+            }
+            if (_Slices.Count < 1) {
+                MessageBox.Show("Can't find Dicom image");
+                return false;
+            }
+            TbrSliceIdx.Minimum = 0;
+            TbrSliceIdx.Maximum = _Slices.Count - 1;
+            TbrSliceIdx.Value = 0;
 
-            var image = new DicomImage(@"D:\usr\prog\PedicleLength\SampleData\5mm early phase\pedicle length 5mm early001");
-            var data = image.PixelData;
-            Debug.WriteLine(data.NumberOfFrames);
-            var buf = data.GetFrame(0);
-            Debug.WriteLine(buf.Size);
+            return true;
+        }
 
-            Mat frame = new Mat(512, 512, MatType.CV_16SC1, buf.Data);
-            double minV, maxV;
-            frame.MinMaxIdx(out minV, out maxV);
-            Debug.WriteLine($"min {minV} max {maxV}");
+        /// <summary>
+        /// 描画
+        /// </summary>
+        private void Draw() {
+            if (_Slices.Count < 1) return;  // データ読み込み前は表示しない
+
+            // パラメータ設定
+            double alpha = 255.0 / _ww;
+            double beta = -alpha * (_wl - _ww / 2);
+
+            // 画像を表示用に変換（Window処理含む）
             var frame8 = new Mat();
-            frame.ConvertTo(frame8, MatType.CV_8UC1, 1, 0);
-            pictureBox1.Image = BitmapConverter.ToBitmap(frame8);
+            _Slices[_idx].ConvertTo(frame8, MatType.CV_8UC1, alpha, beta);
+            var frameC = new Mat();
+            Cv2.CvtColor(frame8, frameC, ColorConversionCodes.GRAY2BGR);
 
+            // 点の描画
+            for (int i = 0; i < _Points.Count; i++) {
+                if (_Points[i].Z == _idx) {
+                    Cv2.Circle(frameC, new Point(_Points[i].X, _Points[i].Y), PointSize, new Scalar(255, 0, 0), Cv2.FILLED);
+                    Cv2.PutText(frameC, $"{i + 1}", new Point(_Points[i].X + PointSize, _Points[i].Y + PointSize),
+                        HersheyFonts.HersheySimplex, 0.4, new Scalar(255, 0, 0));
+                }
+            }
+            // 画像表示
+            pictureBox1.Image = BitmapConverter.ToBitmap(frameC);
+
+            // ラベル更新
+            LblVoxel.Text = $"Voxel: {_PixelSpacingX:0.000} x {_PixelSpacingY:0.000} x {_SliceThickness:0.0} mm";
+            LblSliceIdx.Text = $"Slice Index: {_idx}/{_Slices.Count - 1}";
+            LblWindowLevel.Text = $"Window Level: {_wl}";
+            LblWindowWidth.Text = $"Window Width: {_ww}";
+        }
+
+
+        /// <summary>
+        /// スライダー移動時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SliderChanged(object sender, EventArgs e) {
+            _idx = TbrSliceIdx.Value;
+            _wl = TbrWindowLevel.Value;
+            _ww = TbrWindowWidth.Value;
+            this.Draw();
+        }
+
+        /// <summary>
+        /// Open Dicom Folderボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnOpenDicom_Click(object sender, EventArgs e) {
+            if (!ReadDicom(@"D:\usr\prog\PedicleLength\SampleData\5mm early phase")) return;
+            this.Draw();
+        }
+
+        /// <summary>
+        /// Zoomモードでピクチャボックスに表示した画像の座標を取得する
+        /// </summary>
+        /// <param name="pb"></param>
+        /// <param name="px"></param>
+        /// <param name="py"></param>
+        /// <returns></returns>
+        private Point ImagePosOnPbox(PictureBox pb, int px, int py) {
+            var pos = new Point(-1, -1);
+            int pw = pb.Width;
+            int ph = pb.Height;
+            if (pb.Image == null) return pos;
+            int iw = pb.Image.Width;
+            int ih = pb.Image.Height;
+
+            if (pw * ih > ph * iw) { // PBox横長
+                var piw = ph * iw / ih;
+                var margin = (pw - piw) / 2;
+                if (px < margin || px >= pw - margin) return pos;
+                pos.Y = ih * py / ph;
+                pos.X = iw * (px - margin) / piw;
+            } else {    // PBox縦長
+                var pih = pw * ih / iw;
+                var margin = (ph - pih) / 2;
+                if (py < margin || py >= ph - margin) return pos;
+                pos.X = iw * px / pw;
+                pos.Y = ih * (py - margin) / pih;
+            }
+
+            return pos;
+        }
+
+        /// <summary>
+        /// ピクチャボックス上のマウス移動
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            var pos = ImagePosOnPbox((PictureBox)sender, e.X, e.Y);
+            short ctval = -1;
+            if (pos.X >= 0) ctval = _Slices[_idx].At<short>(pos.Y, pos.X);
+            LblCursor.Text = $"Cursor ({pos.X}, {pos.Y}) : {ctval}";
+        }
+
+        /// <summary>
+        /// ピクチャボックス上のホイール操作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e) {
+            if (e.Delta < 0 && TbrSliceIdx.Value < TbrSliceIdx.Maximum) {
+                TbrSliceIdx.Value++;
+            }
+            if (e.Delta > 0 && TbrSliceIdx.Value > TbrSliceIdx.Minimum) {
+                TbrSliceIdx.Value--;
+            }
+        }
+
+        /// <summary>
+        /// ピクチャボックス上のマウスクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
+            var pos = ImagePosOnPbox((PictureBox)sender, e.X, e.Y);
+            if (pos.X < 0) return;  // 画像外なら終了
+            _Points.Add(new Point3i(pos.X, pos.Y, _idx));
+            this.UpdateList();
+            this.Draw();
+        }
+
+        /// <summary>
+        /// 選択点リストの更新
+        /// </summary>
+        private void UpdateList() {
+            LbxPoints.Items.Clear();
+            double length = 0;
+            for (var i = 0; i < _Points.Count; i++) {
+                LbxPoints.Items.Add($"{i + 1,2} ({_Points[i].X,3}, {_Points[i].Y,3}, {_Points[i].Z,4})");
+                if (i > 0) {
+                    var dx = (_Points[i].X - _Points[i - 1].X) * _PixelSpacingX;
+                    var dy = (_Points[i].Y - _Points[i - 1].Y) * _PixelSpacingY;
+                    var dz = (_Points[i].Z - _Points[i - 1].Z) * _SliceThickness;
+                    length += Math.Sqrt(dx * dx + dy * dy + dz * dz);
+                }
+            }
+            Debug.WriteLine(length);
+            LblLength.Text = $"{length,6:0.0}";
         }
     }
 }
